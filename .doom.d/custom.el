178ef881dea277a035f9cdea373f3a45ab30e601
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-11-18 18:24:55 +0200

    Update custom.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-types '((initialization) (initialization) (defvaralias))))
+ '(warning-suppress-types '((initialization) (initialization) (defvaralias)))
+ '(org-file-apps
+    '(("\\.pdf\\'" . emacs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
