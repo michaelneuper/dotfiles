26092b3d2cf0b6727c894a2c3ac2fb6c31371e34
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2022-12-02 20:33:36 +0200

    Completely change polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,12 +11,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 # polybar -q main -c "$DIR"/config.ini &
-polybar -q workspaces &
-polybar -q win-title &
-polybar -q music &
-polybar -q info &
-polybar -q time &
-polybar -q power &
+# polybar -q workspaces &
+# polybar -q win-title &
+# polybar -q music &
+# polybar -q info &
+# polybar -q time &
+# polybar -q power &
+polybar -q mybar &
 
 # IPC settings and test
 ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
