fa9827257d3a1f5d22c03341bffe4f6a2d095d11
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2023-05-25 19:58:19 +0200

    Switch from doom-dashboard to dashboard.el

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -50,7 +50,7 @@
        ophints           ; highlight the region an operation acts on
        (popup
         +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;tabs              ; a tab bar for Emacs
+       ;; tabs              ; a tab bar for Emacs
        (treemacs
         +lsp)          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
