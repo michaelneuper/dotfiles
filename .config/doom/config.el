70c1229f859e05217109419fb13018d380e60696
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2023-06-11 16:31:49 +0200

    Inset table of contents

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,3 +1,34 @@
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+
+;;   (after! PACKAGE
+;;     (setq x y))
+
+;; The exceptions to this rule:
+;;     - Setting file/directory variables (like `org-directory')
+;;     - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;     - Setting doom variables (which start with 'doom-' or '+').
+
+;; Here are some additional functions/macros that will help you configure Doom.
+
+;;     - `load!' for loading external *.el files relative to this one
+;;     - `use-package!' for configuring packages
+;;     - `after!' for running code after a package has loaded
+;;     - `add-load-path!' for adding directories to the `load-path', relative to
+;;     this file. Emacs searches the `load-path' when you load packages with
+;;     `require' or `use-package'.
+;;     - `map!' for binding new keys
+
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
 (setq doom-theme 'doom-one)
 
 (setq doom-localleader-key "SPC l"
@@ -231,6 +262,14 @@
 (add-to-list 'org-latex-packages-alist '("" "amsmath" t))
 (add-to-list 'org-latex-packages-alist '("" "amssymb" t))
 (setq org-preview-latex-default-process 'dvipng)
+
+(map! :after latex
+      :map latex-mode-map
+      :localleader
+      :desc "Insert math symbol"
+      "i" #'cdlatex-math-symbol
+      :desc "Begin environment"
+      "e" #'cdlatex-environment)
 
 (map! :leader
       (:prefix ("e" . "evaluate")
