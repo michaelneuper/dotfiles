b5898b5b07a29f0d2886487c10264be5aae85c7f
Michael Neuper <73108749+michaelneuper@users.noreply.github.com>
2023-06-11 15:34:04 +0200

    Change to literate config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -226,7 +226,7 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default
          +bindings
          +smartparens))
