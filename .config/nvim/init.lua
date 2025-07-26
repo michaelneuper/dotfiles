5c2a6d92d6a7fe3040c1e524c3e27f97a658727e
Michael Neuper <michael@michaelneuper.com>
2025-07-26 21:56:40 +0200

    update nvim and zsh config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,125 @@
-vim.cmd.colorscheme('gruvbox')
+-- OPTIONS
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
-vim.o.termguicolors = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile = false
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+-- auto remove trailing whitespace
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
 
-vim.o.smartcase = true
-vim.o.ignorecase = true
-vim.o.hlsearch = false
+-- PACKAGE MANAGER
+vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
-vim.g.mapleader = vim.keycode('<Space>')
+require("lazy").setup({
+  { "ellisonleao/gruvbox.nvim" },
+  { "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    } },
+  { "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    } },
+  { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate" },
+  { "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true },
+})
+
+-- vim.pack.add( {
+--     "ellisonleao/gruvbox.nvim",
+--     "nvim-lualine/lualine.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "nvim-telescope/telescope.nvim",
+--     "nvim-tree/nvim-web-devicons",
+--     "nvim-treesitter/nvim-treesitter",
+--     "windwp/nvim-autopairs",
+-- })
+
+-- KEYBINDINGS
+vim.g.mapleader = vim.keycode("<Space>")
+
 vim.keymap.set("n", "<leader>fs", ":w<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>qq", ":q<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>w", "<C-w>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "c" }, "<C-g>", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ot", ":botright split | resize 10 | terminal<CR>",{
+  noremap = true, silent = true, desc = "Open terminal"
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find File" })
+
+-- UI
+vim.opt.termguicolors = true
+
+require("gruvbox").setup({
+  terminal_colors = false,
+
+  underline = true,
+  italic = {
+    strings = false,
+    comments = true,
+    folds = true,
+  },
+})
+
+vim.cmd.colorscheme("gruvbox")
+
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "filename" },
+    lualine_c = { "location", "progress" },
+
+    lualine_x = { "encoding", "filetype" },
+    lualine_y = { "diff" },
+    lualine_z = { "branch" },
+  },
+
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = { "filename" },
+    lualine_c = { "location" },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+
+  tabline = {},
+  extensions = {},
+})
--- .zshrc
+++ .zshrc
@@ -1,21 +1,15 @@
-# Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-# Cool themes: nicoulaj, refined, robbyrussell
-# ZSH_THEME="refined"
 unset ZSH_THEME
 
 CASE_SENSITIVE="false"
 HYPHEN_INSENSITIVE="true"
 
-zstyle ':omz:update' mode reminder # just remind me to update when it's time
-
-zstyle ':omz:update' frequency 15
+zstyle ":omz:update" mode reminder # just remind me to update when it's time
+zstyle ":omz:update" frequency 15
 
 COMPLETION_WAITING_DOTS="true"
 
-plugins=(starship git vi-mode colored-man-pages macos zsh-syntax-highlighting zsh-autosuggestions)
+plugins=(starship zoxide git vi-mode colored-man-pages macos zsh-syntax-highlighting zsh-autosuggestions fzf)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -23,29 +17,61 @@ source $ZSH/oh-my-zsh.sh
 VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
 VI_MODE_SET_CURSOR=true
 
+bindkey "^J" down-line-or-search # or down-line-or-history
+bindkey "^K" up-line-or-search   # or up-line-or-history
+bindkey "^B" backward-char
+bindkey "^F" forward-char
+
 HISTFILE=~/.zhistory
 HISTSIZE=50000
 SAVEHIST=10000
 
 # Env variables
-
-# set $PATH if ~/.local/bin exist
-if [ -d "$HOME/.local/bin" ]; then
-    export PATH=$HOME/.local/bin:$PATH
-fi
-
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.config/emacs/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 
 export LIBGS="/opt/homebrew/lib/libgs.dylib" # fix LaTeX rendering in emacs
 
-# Aliases
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+--height 60%
+--style full
+--walker-skip .git,node_modules,target
+--bind 'ctrl-/:change-preview-window(down|hidden|)'
+--border sharp
+--layout reverse
+--prompt '∷ '
+--pointer ▶
+--marker ⇒
+--color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
+--color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
 
+export FZF_CTRL_T_OPTS="
+--walker-skip .git,node_modules,target
+--preview 'bat -n --color=always --theme=gruvbox-dark {}'
+--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+# Aliases
 if command -v bat >/dev/null 2>&1; then
     alias cat="bat --plain --theme=gruvbox-dark"
 fi
 
-alias v="vim"
+if command -v zoxide >/dev/null 2>&1; then
+    alias cd="z"
+fi
+
+if command -v lsd >/dev/null 2>&1; then
+    alias ls="lsd -F"
+    alias ll="lsd -Fl"
+else
+    alias ls="ls -GF"
+    alias ll="ls -GFl"
+fi
+
+alias home="cd ~/"
+
+alias e="nvim"
+alias v="nvim"
 alias vim="emacs -nw"
 
 if command -v git-st >/dev/null 2>&1; then
@@ -53,6 +79,9 @@ if command -v git-st >/dev/null 2>&1; then
 else
     alias gs="git status --short --branch"
 fi
+
+alias cs="gh copilot suggest"
+alias ce="gh copilot explain"
 
 alias gst="git status"
 alias ga="git add"
@@ -64,3 +93,8 @@ alias gc="git commit -m"
 alias gp="git pull"
 alias gpu="git push"
 alias gl="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias bs="brew search"
+alias bi="brew install"
+alias bic="brew install --cask"
+alias bu="brew update && brew upgrade"
